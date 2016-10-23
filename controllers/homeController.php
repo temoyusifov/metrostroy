5a0d6742dc8d128060b93e350e21550f096df161
Temo Yusifov <temoyusifov@gmail.com>
2016-10-23 17:56:54 +0400

    update homeController

--- controllers/homeController.php
+++ controllers/homeController.php
@@ -6,6 +6,12 @@
 
     class Home extends coreControllers\AppCoreController{
 
+        // IF CALLED METHOD THAT NOT EXISTS
+        public function __call($methodName, $methodArguments){
+            $this->load->view('404');
+        }
+
+        // CONTROLLER DEFAULT METHOD
         public function main(){
 
             // Some statement
@@ -16,13 +22,13 @@
                 'RUS' => 'Russian'
             );
 
-            $this->load->library('load'); // This line for load module when we get library object
+           // $this->load->library('load'); // This line for load module when we get library object
 
             $this->load->model('usersModel');
             $usersObject = $this->usersModel->getUsers(); // returns object
 
             
-            //$this->load->view('welcome', $languages);
+            $this->load->view('welcome', $languages);
            // $this->load->view('welcome2', $languages);
            // $this->load->view('welcome3', $languages);
 
